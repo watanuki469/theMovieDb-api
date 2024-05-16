907e85d94ff6844f33370355bd7332a29a1a6128
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-05-16 16:27:37 +0700

    fix regis

--- index.js
+++ index.js
@@ -5,34 +5,21 @@ const RegisterModel = require('./models/Register')
 
 const app = express()
 app.use(cors());
-  
+
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://admin:vNyLVQug8B7quWE4@cluster0.3avkh2c.mongodb.net/theMovieDb');
 
-
 app.get("/", (req, res) => {
     res.json("Welcome to Vasiliev Movie Database");
 })
-// app.post('/register', (req, res) => {
-//     const {name, email, password} = req.body;
-//     RegisterModel.findOne({email: email})
-//     .then(user => {
-//         if(user) {
-//             res.json("Already have an account")
-//         } else {
-//             RegisterModel.create({name: name, email: email, password: password})
-//             .then(result => res.json(result))
-//             .catch(err => res.json(err))
-//         }
-//     }).catch(err => res.json(err))
-// })
 app.post('/register', async (req, res) => {
     try {
         const { name, email, password } = req.body;
-        const checkUser = await userModel.findOne({ email });
-        if (checkUser) return res.json("Email Already Used");
-        else {
+        const checkUser = await RegisterModel.findOne({ email });
+        if (checkUser) {
+            return res.status(400).json({ message: "Email Already Used" });
+        } else {
             RegisterModel.findOne({ email: email })
                 .then(user => {
                     if (user) {
@@ -43,7 +30,7 @@ app.post('/register', async (req, res) => {
                             .catch(err => res.json(err))
                         res.status(201).json({ message: 'Người dùng đã được tạo thành công.' });
                     }
-                }).catch(err => res.json(err))
+                })
         }
     }
     catch (error) {
