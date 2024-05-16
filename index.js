3d8f5ac626fbe3379d727087972f8a963d1e310e
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-05-16 16:02:10 +0700

    fix register

--- index.js
+++ index.js
@@ -4,33 +4,55 @@ const cors = require('cors')
 const RegisterModel = require('./models/Register')
 
 const app = express()
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+app.use(cors({
+    origin: 'http://127.0.0.1:5173', // Allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  }));
+  
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://admin:vNyLVQug8B7quWE4@cluster0.3avkh2c.mongodb.net/theMovieDb');
 
 
 app.get("/", (req, res) => {
-    res.json("Hello");
+    res.json("Welcome to Vasiliev Movie Database");
 })
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then(user => {
-        if(user) {
-            res.json("Already have an account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
+// app.post('/register', (req, res) => {
+//     const {name, email, password} = req.body;
+//     RegisterModel.findOne({email: email})
+//     .then(user => {
+//         if(user) {
+//             res.json("Already have an account")
+//         } else {
+//             RegisterModel.create({name: name, email: email, password: password})
+//             .then(result => res.json(result))
+//             .catch(err => res.json(err))
+//         }
+//     }).catch(err => res.json(err))
+// })
+app.post('/register', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const checkUser = await userModel.findOne({ email });
+        if (checkUser) return res.json("Email Already Used");
+        else {
+            RegisterModel.findOne({ email: email })
+                .then(user => {
+                    if (user) {
+                        res.json("Already have an account")
+                    } else {
+                        RegisterModel.create({ name: name, email: email, password: password })
+                            .then(result => res.json(result))
+                            .catch(err => res.json(err))
+                        res.status(201).json({ message: 'Người dùng đã được tạo thành công.' });
+                    }
+                }).catch(err => res.json(err))
         }
-    }).catch(err => res.json(err))
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Đã xảy ra lỗi trong quá trình tạo người dùng.' });
+    }
 })
 
 
