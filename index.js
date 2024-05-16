544bca3151c3ce61645e3ec797fc91c6a69332e1
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-05-16 16:35:45 +0700

    text new register login

--- index.js
+++ index.js
@@ -13,31 +13,46 @@ mongoose.connect('mongodb+srv://admin:vNyLVQug8B7quWE4@cluster0.3avkh2c.mongodb.
 app.get("/", (req, res) => {
     res.json("Welcome to Vasiliev Movie Database");
 })
+// app.post('/register', async (req, res) => {
+//     try {
+//         const { name, email, password } = req.body;
+//         const checkUser = await RegisterModel.findOne({ email });
+//         if (checkUser) {
+//             return res.status(400).json({ message: "Email Already Used" });
+//         } else {
+//             RegisterModel.findOne({ email: email })
+//                 .then(user => {
+//                     if (user) {
+//                         res.json("Already have an account")
+//                     } else {
+//                         RegisterModel.create({ name: name, email: email, password: password })
+//                             .then(result => res.json(result))
+//                             .catch(err => res.json(err))
+//                         res.status(201).json({ message: 'Người dùng đã được tạo thành công.' });
+//                     }
+//                 })
+//         }
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: 'Đã xảy ra lỗi trong quá trình tạo người dùng.' });
+//     }
+// })
 app.post('/register', async (req, res) => {
     try {
         const { name, email, password } = req.body;
         const checkUser = await RegisterModel.findOne({ email });
+
         if (checkUser) {
             return res.status(400).json({ message: "Email Already Used" });
         } else {
-            RegisterModel.findOne({ email: email })
-                .then(user => {
-                    if (user) {
-                        res.json("Already have an account")
-                    } else {
-                        RegisterModel.create({ name: name, email: email, password: password })
-                            .then(result => res.json(result))
-                            .catch(err => res.json(err))
-                        res.status(201).json({ message: 'Người dùng đã được tạo thành công.' });
-                    }
-                })
+            const newUser = await RegisterModel.create({ name, email, password });
+            return res.status(201).json(newUser);
         }
+    } catch (error) {
+        console.error(error);
+        // res.status(500).json({ message: 'Đã xảy ra lỗi trong quá trình tạo người dùng.' });
     }
-    catch (error) {
-        res.status(500).json({ message: 'Đã xảy ra lỗi trong quá trình tạo người dùng.' });
-    }
-})
-
+});
 
 app.listen(3001, () => {
     console.log("Server is Running")
