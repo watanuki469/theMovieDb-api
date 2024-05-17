25acd0c42b8805627d1389fa43732b10ed821af4
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-05-17 13:13:57 +0700

    test origin2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const userRoutes = require('./routes'); // Corrected path
 
 const app = express();
 app.use(cors({
-    origin: true, //  frontend URL
+    origin: 'http://127.0.0.1:5173/', //  frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
