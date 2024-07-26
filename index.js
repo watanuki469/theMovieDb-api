23ac5d16bb0b7a1bee701654f45de718077fc669
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-07-26 10:48:06 +0700

    change to local

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const userRoutes = require('./src/routes'); 
 const app = express();
 const corsOptions = {
-    origin: 'https://themoviedb-five.vercel.app',
+    origin: 'http://127.0.0.1:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
