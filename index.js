5780de93e08d09a6932364d4b80abdb5f29d2ea7
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-07-26 10:13:42 +0700

    change to vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const userRoutes = require('./src/routes'); 
 const app = express();
 const corsOptions = {
-    origin: 'http://127.0.0.1:5173',
+    origin: 'https://themoviedb-five.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
